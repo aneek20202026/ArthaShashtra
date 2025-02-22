5252a3fd50d32fae51e44d132ae8e0e15860fdf2
Aneek <aneekmallick20@gmail.com>
2025-02-22 17:00:27 +0530

    Initial commit

--- settings.py
+++ settings.py
@@ -4,8 +4,10 @@ from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
+SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
