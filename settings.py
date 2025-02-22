68e054be2afb67a4d72853fd18c423ff55b6cb2c
Aneek <aneekmallick20@gmail.com>
2025-02-22 16:56:04 +0530

    Second Commit

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Apple@localhost/finance'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
